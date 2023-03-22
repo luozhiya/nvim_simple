8002fa50c83c324ed44268f0941454d080908b49
luozhiya <luozhiya@petalmail.com>
2023-03-22 17:10:31 +0800

    try lazy

--- lua/module/plugins.lua
+++ lua/module/plugins.lua
@@ -4,9 +4,9 @@ return {
   { 'nvim-lua/plenary.nvim' },
   { 'folke/tokyonight.nvim', lazy = false, priority = 1000, config = config('folke/tokyonight.nvim') },
   --- Treesitter
-  { 'nvim-treesitter/nvim-treesitter', config = config('nvim-treesitter/nvim-treesitter') },
+  { 'nvim-treesitter/nvim-treesitter', event = 'BufReadPost', config = config('nvim-treesitter/nvim-treesitter') },
   --- Telescope
-  { 'nvim-telescope/telescope.nvim', config = config('nvim-telescope/telescope.nvim') },
+  { 'nvim-telescope/telescope.nvim', cmd = { 'Telescope' }, config = config('nvim-telescope/telescope.nvim') },
   { 'nvim-telescope/telescope-ui-select.nvim' },
   { 'nvim-telescope/telescope-fzf-native.nvim', build = 'cmake -S. -Bbuild -DCMAKE_BUILD_TYPE=Release && cmake --build build --config Release && cmake --install build --prefix build' },
   { 'debugloop/telescope-undo.nvim' },
@@ -17,10 +17,10 @@ return {
   { 'stevearc/aerial.nvim', config = config('stevearc/aerial.nvim') },
   { 'ahmedkhalf/project.nvim', config = config('ahmedkhalf/project.nvim') },
   --- Completion
-  { 'hrsh7th/nvim-cmp', config = config('hrsh7th/nvim-cmp') },
-  { 'hrsh7th/cmp-cmdline' },
-  { 'hrsh7th/cmp-buffer' },
-  { 'hrsh7th/cmp-path' },
+  { 'hrsh7th/nvim-cmp', event = 'BufReadPost', config = config('hrsh7th/nvim-cmp') },
+  { 'hrsh7th/cmp-cmdline', event = { 'CmdlineEnter', 'InsertEnter' } },
+  { 'hrsh7th/cmp-buffer', event = { 'CmdlineEnter', 'InsertEnter' } },
+  { 'hrsh7th/cmp-path', event = { 'CmdlineEnter', 'InsertEnter' } },
   { 'hrsh7th/cmp-nvim-lsp' },
   { 'L3MON4D3/LuaSnip' },
   { 'saadparwaiz1/cmp_luasnip' },
@@ -31,21 +31,21 @@ return {
   { 'stevearc/dressing.nvim', config = config('stevearc/dressing.nvim') },
   --- Git
   { 'lewis6991/gitsigns.nvim', config = config('lewis6991/gitsigns.nvim') },
-  { 'akinsho/toggleterm.nvim', config = config('akinsho/toggleterm.nvim') },
   --- Buffer
   { 'qpkorr/vim-bufkill' },
   { 'moll/vim-bbye' },
   { 'kazhala/close-buffers.nvim', config = config('kazhala/close-buffers.nvim') },
   --- Key Management
-  { 'folke/which-key.nvim', config = config('folke/which-key.nvim') },
-  { 'mrjones2014/legendary.nvim', config = config('mrjones2014/legendary.nvim') },
+  { 'folke/which-key.nvim', keys = { { ',' }, { 'g' } }, config = config('folke/which-key.nvim') },
+  { 'mrjones2014/legendary.nvim', event = 'VeryLazy', config = config('mrjones2014/legendary.nvim') },
   --- UI
-  { 'j-hui/fidget.nvim', config = config('j-hui/fidget.nvim') },
-  { 'nvim-lualine/lualine.nvim', config = config('nvim-lualine/lualine.nvim') },
-  { 'nvim-tree/nvim-tree.lua', config = config('nvim-tree/nvim-tree.lua') },
+  { 'j-hui/fidget.nvim', event = 'VeryLazy', config = config('j-hui/fidget.nvim') },
+  { 'nvim-lualine/lualine.nvim', event = 'VeryLazy', config = config('nvim-lualine/lualine.nvim') },
+  { 'nvim-tree/nvim-tree.lua', event = 'VeryLazy', config = config('nvim-tree/nvim-tree.lua') },
+  { 'akinsho/toggleterm.nvim', cmd = { 'ToggleTerm', 'ToggleTerminalGitUI', 'ToggleTerminalLazyGit' }, config = config('akinsho/toggleterm.nvim') },
   --- CmdLine
   { 'VonHeikemen/fine-cmdline.nvim', config = config('VonHeikemen/fine-cmdline.nvim'), cmd = { 'FineCmdline' } },
-  { 'gelguy/wilder.nvim', config = config('gelguy/wilder.nvim') },
+  { 'gelguy/wilder.nvim', keys = { { '?' } }, config = config('gelguy/wilder.nvim') },
   --- Edit
   { 'tpope/vim-obsession' },
   { 'windwp/nvim-autopairs' },
@@ -54,7 +54,7 @@ return {
   { 'ray-x/lsp_signature.nvim', config = config('ray-x/lsp_signature.nvim') },
 
   -- LSP
-  { 'neovim/nvim-lspconfig', config = require('module.lsp').setup },
+  { 'neovim/nvim-lspconfig', event = 'BufReadPost', ft = { 'c', 'cpp', 'lua' }, config = require('module.lsp').setup },
   { 'williamboman/mason.nvim' },
   { 'williamboman/mason-lspconfig.nvim' },
   { 'p00f/clangd_extensions.nvim' },
